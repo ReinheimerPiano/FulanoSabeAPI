090c2d13e5709bf8243231a578f0e9dff8e3d913
ReinheimerPiano <pianomatheus@gmail.com>
2018-11-28 19:01:12 -0200

    teste unit

---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestInitialize]
+        public void IniciarTestes()
+        {
+
+        }
+        [TestMethod]
+        public void TesteConectar()
+        {
+
+        }
+    }
+}
